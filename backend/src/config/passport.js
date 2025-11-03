d19654527141f08b329de2ab88b13061988975ef
Richard Gabelman <78068360+RichardGabelman@users.noreply.github.com>
2025-11-03 10:36:25 -0800

    feat: implement github auth and jwt strategies

---
+++ backend/src/config/passport.js
@@ -0,0 +1,70 @@
+import passport from "passport";
+import { Strategy as GithubStrategy} from "passport-github2";
+import prisma from "../db/prisma";
+import { Strategy as JwtStrategy } from "passport-jwt";
+import { ExtractJwt } from "passport-jwt";
+
+passport.use(
+  new GithubStrategy(
+    {
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      callbackURL: process.env.GITHUB_CALLBACK_URL || 'http://localhost:3000/api/auth/github/callback',
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        let user = await prisma.user.findUnique({
+          where: {githubId: profile.id },
+        });
+
+        if (!user) {
+          user = await prisma.user.create({
+            data: {
+              githubId: profile.id,
+              username: profile.username,
+              displayName: profile.displayName || profile.username,
+              bio: profile.bio || null,
+              avatarUrl: profile.photos?.[0]?.value || null,
+            },
+          });
+        }
+
+        return done(null, user);
+      } catch (error) {
+        return done(error, null);
+      }
+    }
+  )
+);
+
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.JWT_SECRET,
+};
+
+passport.use(
+  new JwtStrategy(jwtOptions, async function (jwt_payload, done) {
+    try {
+      const user = await prisma.user.findUnique({
+        where: {
+          id: jwt_payload.userId,
+        },
+        select: {
+          id: true,
+          username: true,
+          displayName: true,
+        },
+      });
+
+      if (user) {
+        return done(null, user);
+      } else {
+        return done(null, false);
+      }
+    } catch (error) {
+      return done(error);
+    }
+  })
+);
+
+export default passport;
