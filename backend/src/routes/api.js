4470aaf1dd6279dfd315c9451444736675ea86cf
Richard Gabelman <78068360+RichardGabelman@users.noreply.github.com>
2025-11-07 19:20:38 -0800

    feat: implement user index fetching

--- backend/src/routes/api.js
+++ backend/src/routes/api.js
@@ -3,6 +3,7 @@ import authRouter from "./auth.js";
 import followRouter from "./follows.js";
 import postRouter from "./posts.js";
 import profileRouter from "./profile.js";
+import userRouter from "./users.js";
 
 const router = express.Router();
 
@@ -13,5 +14,7 @@ router.use("/follows", followRouter);
 router.use("/posts", postRouter);
 
 router.use("/profile", profileRouter);
+
+router.use("/users", userRouter);
 
 export default router;
