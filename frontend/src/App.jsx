7bab4cbdbef44ce29adb1d9ef5f07a258d0dc1b6
Richard Gabelman <78068360+RichardGabelman@users.noreply.github.com>
2025-11-20 21:58:30 -0800

    feat: add login page and authcallback page routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login.jsx";
+import AuthCallback from "./pages/AuthCallback.jsx";
 import { authService } from "./services/api.js";
 
 function ProtectedRoute({ children }) {
@@ -9,6 +11,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/auth/callback" element={<AuthCallback />} />
       </Routes>
     </BrowserRouter>
   );
