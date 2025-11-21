98e0a67e953a0a8cb94e0b3d25b9bd0ae0473210
Richard Gabelman <78068360+RichardGabelman@users.noreply.github.com>
2025-11-20 20:46:36 -0800

    refactor: remove app.css and change app to use router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,17 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { authService } from "./services/api.js";
+
+function ProtectedRoute({ children }) {
+  return authService.isAuthenticated() ? children : <Navigate to="/login" />;
+}
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
