11e583e1985b2ca237687f81c7a80eda7843e249
Richard Gabelman <78068360+RichardGabelman@users.noreply.github.com>
2025-12-10 16:31:58 -0800

    feat: replace trashcan emoji with svg for post deletion

--- frontend/src/components/PostCard.jsx
+++ frontend/src/components/PostCard.jsx
@@ -108,7 +108,11 @@ function PostCard({ post, onPostDeleted }) {
 
   function CommentSpeechBubble() {
     return (
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="reply post-svg">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        className="reply post-svg"
+      >
         <title>Reply</title>
         <path d="M12,3C6.5,3 2,6.58 2,11C2.05,13.15 3.06,15.17 4.75,16.5C4.75,17.1 4.33,18.67 2,21C4.37,20.89 6.64,20 8.47,18.5C9.61,18.83 10.81,19 12,19C17.5,19 22,15.42 22,11C22,6.58 17.5,3 12,3M12,17C7.58,17 4,14.31 4,11C4,7.69 7.58,5 12,5C16.42,5 20,7.69 20,11C20,14.31 16.42,17 12,17Z" />
       </svg>
@@ -173,7 +177,13 @@ function PostCard({ post, onPostDeleted }) {
                       onClick={handleDelete}
                     >
                       <span>Delete</span>
-                      <div>🗑️</div>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                      >
+                        <title>Delete</title>
+                        <path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z" />
+                      </svg>
                     </button>
                   </div>
                 )}
