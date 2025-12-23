e625b26c18425b722def8e9c44929150fc02f515
Richard Gabelman <78068360+RichardGabelman@users.noreply.github.com>
2025-12-22 20:53:52 -0800

    fix: mobile header now sticks to the top

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -142,7 +142,18 @@ function Layout({
       </nav>
 
       <div className="center-area">
-        <div className="mobile-home-header">
+        <header className="mobile-home-header">
+          {showBackButton && (
+            <button className="mobile-back-button" onClick={handleBack}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                className="nav-icon"
+              >
+                <path d="M20,11V13H8L13.5,18.5L12.08,19.92L4.16,12L12.08,4.08L13.5,5.5L8,11H20Z" />
+              </svg>
+            </button>
+          )}
           <div className="mobile-logo-header">
             <Link to="/" className="logo">
               <svg
@@ -175,7 +186,7 @@ function Layout({
               </button>
             </div>
           )}
-        </div>
+        </header>
         <header className="top-header">
           <div className="header-left">
             {showBackButton && (
