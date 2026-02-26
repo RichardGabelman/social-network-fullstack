cbf1191cc76b06989d883549e11d286d03c0af42
Richard Gabelman <78068360+RichardGabelman@users.noreply.github.com>
2026-02-26 08:18:42 -0800

    refactor: move import of config file above other imports

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,8 @@
+import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import apiRouter from "./src/routes/api.js";
 
-import "dotenv/config";
 
 const app = express();
 
